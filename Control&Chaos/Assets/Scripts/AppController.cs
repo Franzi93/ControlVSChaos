ced6e1f9324353d2e92e99b603dabc9a5b29dc5c
Franzi <fneu93@googlemail.com>
2022-01-21 22:22:15 +0100

    added debug controller as prefab

--- Control&Chaos/Assets/Scripts/AppController.cs
+++ Control&Chaos/Assets/Scripts/AppController.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Dmdrn.UnityDebug;
 
 public class AppController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        DebugController.instance.AddAction("test", () => { Debug.Log("test"); });
     }
 
     // Update is called once per frame
