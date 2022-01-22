df277292312247f5d0f1cabff7ede5bd629bcdd3
Tobias Heiles <contact@tobiasheiles.de>
2022-01-22 19:45:28 +0100

    RenderGrid

--- Control&Chaos/Assets/Scripts/RenderGrid/RenderGrid.cs
+++ Control&Chaos/Assets/Scripts/RenderGrid/RenderGrid.cs
@@ -1,21 +1,100 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using Random = System.Random;
 
 namespace Duality
 {
     public class RenderGrid : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] private List<GameObject> cellPrefabs;
+        [SerializeField] private int width;
+        [SerializeField] private int height;
+        [SerializeField] private Vector2 cellSize = new Vector2(2, 2);
         
+        
+        private readonly List<GameObject> cells = new List<GameObject>();
+
+        private void Awake()
+        {
+            FillTestGrid();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Clear()
         {
-        
+            foreach (GameObject cell in cells)
+            {
+                Destroy(cell);
+            }
+            cells.Clear();
+        }
+
+        public void FillNewGrid(int width, int height, bool fromEditor = false)
+        {
+            if (!fromEditor)
+            {
+                Clear();
+            }
+
+            if (cellPrefabs.Count == 0)
+            {
+                Debug.LogError("No tile prefabs set!");
+                return;
+            }
+            
+            Random rnd = new Random();
+            
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < this.width; x++)
+                {
+                    int randomIndex = rnd.Next(cellPrefabs.Count);
+                    Vector2 position = new Vector2(x, y) * cellSize + cellSize / 2.0f;
+                    GameObject cell =  Instantiate(cellPrefabs[randomIndex], transform.position + new Vector3(position.x, 0, position.y), Quaternion.identity, transform);
+                    cells.Add(cell);
+                }   
+            }
+
+            this.width = width;
+            this.height = height;
+        }
+
+        [ContextMenu("FillTestGrid")]
+        public void FillTestGrid()
+        {
+            FillNewGrid(10, 10, true);
+        }
+    }
+
+    
+    [CustomEditor(typeof(RenderGrid))]
+    class RenderGridEditor : Editor
+    {
+        private SerializedProperty grid;
+
+        private void OnEnable()
+        {
+            grid = serializedObject.FindProperty("grid");
+            
+            
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            if (GUILayout.Button("Clear"))
+            {
+                
+            }
+            
+            if (GUILayout.Button("FillNewGrid 10, 10"))
+            {
+                
+                // ((grid.GetType()) grid).FillNewGrid(10, 10);
+            }
         }
     }
 }
